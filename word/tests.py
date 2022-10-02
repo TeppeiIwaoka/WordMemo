dccece50d6d552abee73cbac0f9455917443f8eb
TeppeiIwaoka <tep731kaizi@gmail.com>
2022-10-02 19:33:58 +0900

    optimise import modules

--- word/tests.py
+++ word/tests.py
@@ -1,9 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
-from unittest import mock
-from unittest.mock import patch
-from word.views import WordAdd, DictionaryAPIException
 
 from .models import Word
 
